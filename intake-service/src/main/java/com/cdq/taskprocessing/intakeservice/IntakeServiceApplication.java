331994f3c89239724f15354f137e0710f1ddc139
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-01-12 21:07:36 +0100

    initial code structure

---
+++ intake-service/src/main/java/com/cdq/taskprocessing/intakeservice/IntakeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cdq.taskprocessing.intakeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntakeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntakeServiceApplication.class, args);
+	}
+
+}
---
+++ intake-service/src/test/java/com/cdq/taskprocessing/intakeservice/IntakeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdq.taskprocessing.intakeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntakeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
