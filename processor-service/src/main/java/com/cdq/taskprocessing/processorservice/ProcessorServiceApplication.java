4830e6b8d0cca2bcce6869626a2841b7842507c1
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-01-13 18:27:14 +0100

    local environment properties set up for ProcessorServiceApplication

--- processor-service/src/main/java/com/cdq/taskprocessing/processorservice/ProcessorServiceApplication.java
+++ processor-service/src/main/java/com/cdq/taskprocessing/processorservice/ProcessorServiceApplication.java
@@ -2,8 +2,10 @@ package com.cdq.taskprocessing.processorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.cdq.taskprocessing")
 public class ProcessorServiceApplication {
 
 	public static void main(String[] args) {
