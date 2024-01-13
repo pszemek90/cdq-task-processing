705d60153eb6fae1ab7100c6b5d57dde23dec755
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-01-13 18:21:49 +0100

    local environment properties set up

--- intake-service/src/main/java/com/cdq/taskprocessing/intakeservice/IntakeServiceApplication.java
+++ intake-service/src/main/java/com/cdq/taskprocessing/intakeservice/IntakeServiceApplication.java
@@ -2,8 +2,10 @@ package com.cdq.taskprocessing.intakeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.cdq.taskprocessing")
 public class IntakeServiceApplication {
 
 	public static void main(String[] args) {
