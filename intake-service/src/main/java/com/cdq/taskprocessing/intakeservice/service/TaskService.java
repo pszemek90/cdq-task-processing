0ada9e7ccbf3a90a5e1b544e595a528a262ca79e
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2024-01-14 13:24:19 +0100

    More logging added

--- intake-service/src/main/java/com/cdq/taskprocessing/intakeservice/service/TaskService.java
+++ intake-service/src/main/java/com/cdq/taskprocessing/intakeservice/service/TaskService.java
@@ -4,6 +4,8 @@ import com.cdq.taskprocessing.database.dao.TaskDao;
 import com.cdq.taskprocessing.intakeservice.mapper.TaskMapper;
 import com.cdq.taskprocessing.intakeservice.model.Tasks;
 import com.cdq.taskprocessing.model.TaskMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.UUID;
 
 @Service
 public class TaskService {
+
+    private static final Logger log = LoggerFactory.getLogger(TaskService.class);
 
     private final TaskDao taskDao;
     private final KafkaProducerService kafkaProducerService;
@@ -22,7 +26,9 @@ public class TaskService {
 
     public UUID createTask(String input, String pattern) {
         UUID taskId = taskDao.createTask(input, pattern);
+        log.info("Created task with id: {}, for input: {} and pattern: {}", taskId, input, pattern);
         kafkaProducerService.sendMessage(new TaskMessage(taskId, input, pattern));
+        log.debug("Message sent to Kafka topic.");
         return taskId;
     }
 
